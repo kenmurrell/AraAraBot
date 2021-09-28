353d6b23260e2988af08a30f7102c709322b7692
kenmurrell <ken.murrell13@gmail.com>
2021-09-27 21:07:47 -0400

    Upgrade to lock the bot interactions to one channel

--- bot.py
+++ bot.py
@@ -25,7 +25,14 @@ cogs = [Music, Meta]
 config = None
 with open("config.yaml", encoding="utf-8") as configfile:
     config = yaml.load(configfile, Loader=yaml.FullLoader)
+if not config:
+    logger.error("No config.yaml file found!")
+    raise ValueError("No config.yaml file found! Please return the ")
+if config["MAIN"]["TOKEN"] == "":
+    logger.error("No token specified in the config.yaml file")
+    raise ValueError("No token specified in the config.yaml file. Please specify a token.")
 bot = commands.Bot(command_prefix=config["MAIN"]["PREFIX"])
+excl_channel = config["MAIN"]["EXCL_CHANNEL"]
 
 
 @bot.event
@@ -38,12 +45,28 @@ async def on_member_join(user):
     logger.info("{user} joined the server".format(user=user))
 
 
+@bot.event
+async def on_member_join(user):
+    logger.info("{user} joined the server".format(user=user))
+
+
+@bot.event
+async def on_member_remove(user):
+    logger.info("{user} left the server".format(user=user))
+
+
+@bot.event
+async def on_message(message):
+    if message.author.id == bot.user.id or not excl_channel or message.channel.name == excl_channel:
+        await bot.process_commands(message)
+    elif excl_channel not in {ch.name for ch in message.guild.channels}:
+        # Quick check if the user made a mistake specifying their exclusive channel
+        logger.error("The exclusive channel doesnt exist on this server! Maybe check your config file...?")
+
+
 def run():
     for cog in cogs:
         bot.add_cog(cog(bot, config["MUSIC"]))
-    if config["MAIN"]["TOKEN"] == "":
-        logger.error("No token specified in the config.yaml file")
-        raise ValueError("No token specified in the config.yaml file. Please specify a token.")
     bot.run(config["MAIN"]["TOKEN"])
 
 
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 python bot.py
+read p "Application closed. Press any key to close this window."
